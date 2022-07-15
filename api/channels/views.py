3e3588a1910549f45a8b52c8f50ebfc0793683cd
Sanjarbek <sanjarbeksaminjonovv@gmail.com>
2022-07-15 23:29:31 +0500

    change channel message api

--- api/channels/views.py
+++ api/channels/views.py
@@ -17,45 +17,21 @@ def get_formatted_time(now):
 @api_view(['POST'])
 def receive_channel_message(request):
     data = request.data
-    h1 = data.get('h1')
-    h2 = data.get('h2')
-    w1 = data.get('w1')
-    w2 = data.get('w2')
-    vol = data.get('vol')
+    h = data.get('h')
     bat = data.get('bat')
     net = data.get('net')
     latitude = data.get('latitude')
     longitude = data.get('longitude')
-    channel_device_id = data.get('device_id')
+    device_id = data.get('device_id')
 
-    if all((h1, h2, w1, w2, vol, bat, net, channel_device_id)):
-        channel_device = ChannelDevice.objects.filter(device__id=channel_device_id).first()
-        if channel_device is not None:
-            channel_device_message = ChannelMessage.objects.create(
-                channel_device=channel_device,
-                h1=h1,
-                h2=h2,
-                w1=w1,
-                w2=w2,
-                vol=vol,
-                bat=bat,
-                net=net
-            )
-            if all((latitude, longitude)):
-                channel_device.latitude = latitude
-                channel_device.longitude = longitude
-                channel_device.save()
-            return Response(
-                {
-                    'request': 'success',
-                    'height': channel_device.height + channel_device.height_conf,
-                    'phone': channel_device.phone_number,
-                    'datetime': get_formatted_time(
-                        channel_device_message.created_at + datetime.timedelta(hours=5)
-                    )
-                },
-                status=HTTP_201_CREATED
-            )
+    re_settings = data.get('re_settings')
+    print(re_settings)
+
+    if all((h, bat, net, latitude, longitude, device_id)):
+        return Response(
+            {'request': 'success'},
+            status=HTTP_201_CREATED
+        )
     return Response(
         {'request': 'error'},
         status=HTTP_400_BAD_REQUEST
