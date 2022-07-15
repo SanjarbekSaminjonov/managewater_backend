fb87acbd406d106dd22b9778f21b782adc8c10e9
Sanjarbek <sanjarbeksaminjonovv@gmail.com>
2022-07-15 23:33:36 +0500

    Update views.py

--- api/channels/views.py
+++ api/channels/views.py
@@ -24,15 +24,23 @@ def receive_channel_message(request):
     longitude = data.get('longitude')
     device_id = data.get('device_id')
 
-    re_settings = data.get('re_settings')
+    re_settings = data.get('re_settings', False)
     print(re_settings)
 
     if all((h, bat, net, latitude, longitude, device_id)):
         return Response(
-            {'request': 'success'},
+            {
+                'request': 'success',
+                'datetime': str(datetime.datetime.now() + datetime.timedelta(hours=5)),
+                're_settings': re_settings
+            },
             status=HTTP_201_CREATED
         )
     return Response(
-        {'request': 'error'},
+        {
+            'request': 'error',
+            'datetime': str(datetime.datetime.now() + datetime.timedelta(hours=5)),
+            're_settings': re_settings
+        },
         status=HTTP_400_BAD_REQUEST
     )
